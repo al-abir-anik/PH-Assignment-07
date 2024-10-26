5b8713c8370f444dc78f3047af284f3450173d24
al-abir-anik <alabiranik56@gmail.com>
2024-10-26 15:13:30 +0600

    Footer component added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,51 @@
 import Subscription from "./Subscription";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div></div>
-            <Subscription></Subscription>
-            <div>
-
+  return (
+    <footer className="relative bg-[#06091A]">
+      <div>
+        <div className="w-full h-60 bg-white"></div>
+        <Subscription></Subscription>
+        <div className="w-full h-60 "></div>
+      </div>
+      <div className="w-3/4 mx-auto flex flex-col justify-center items-center gap-16">
+        <img src="/public/logo-footer.png" />
+        <div className="grid grid-cols-3 gap-32">
+          <div className="w-auto space-y-3">
+            <h5 className="font-semibold text-white">About Us</h5>
+            <p className="text-white/60 w-72">
+              We are a passionate team dedicated to providing the best services
+              to our customers.
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h5 className="font-semibold text-white mb-2">Quick Links</h5>
+            <a className="text-white/60">Home</a>
+            <a className="text-white/60">Services</a>
+            <a className="text-white/60">About</a>
+            <a className="text-white/60">Contact</a>
+          </div>
+          <div className="space-y-3">
+            <h3 className="font-semibold text-white">Subscribe</h3>
+            <p className="text-white/60">
+              Subscribe to our newsletter for the latest updates.
+            </p>
+            <div className="flex gap-3">
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="input w-full max-w-xs border border-[#131313]/15"
+              />
+              <input type="submit" value="Subscribe" className="btn" />
             </div>
-        </footer>
-    );
+          </div>
+        </div>
+        <p className="text-white/60 w-full h-24 flex justify-center items-center border-t border-white/15">
+          @2024 Your Company All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,8 +1,8 @@
 
 const Subscription = () => {
   return (
-    <div className="w-10/12 p-6 bg-white/15 border-2 rounded-3xl">
-      <div className="w-full h-80 bg-[url('/public/bg-shadow.png')] bg-no-repeat bg-cover rounded-3xl flex flex-col items-center justify-center gap-4">
+    <div className="w-2/3 p-6 bg-white/15 border-2 rounded-3xl  absolute z-10 top-14 left-80">
+      <div className="w-full h-80 bg-white bg-[url('/public/bg-shadow.png')] bg-no-repeat bg-cover rounded-3xl flex flex-col items-center justify-center gap-5">
         <h3 className="text-3xl font-bold text-[#131313]">Subscribe to our Newsletter</h3>
         <p className="text-lg">Get the latest updates and news right in your inbox!</p>
         <div className="flex gap-3">
